778b5e2ee1edb41b3a3f65833c4a24c37cd7b8eb
Jessica <jchung722@gmail.com>
2016-09-27 16:52:46 -0700

    added decoding

--- app/controllers/decodings_controller.rb
+++ app/controllers/decodings_controller.rb
@@ -1,4 +1,6 @@
 class DecodingsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
   end
 
@@ -6,5 +8,6 @@ class DecodingsController < ApplicationController
   end
 
   def show
+    @message = Caesar.decode(params[:shift], params[:message])
   end
 end
